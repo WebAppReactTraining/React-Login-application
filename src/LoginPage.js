50fe0f69401f3921ac907fe3c301f60b955aa7bd
IshantL <ishantlambhate@gmail.com>
2019-04-05 12:29:57 +0000

    adding login page

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 
 class Login extends Component{
+    constructor(props){
+        super();
+        this.state={
+            username:'',
+            password:''
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleSubmit= this.handleSubmit.bind(this);
+    }
+      handleClick(event){
+        console.log("in handleClick");
+        this.setState({username:event.target.value})
+    }
 
+    handleSubmit =e =>{
+        console.log(e.target.value);
+        e.preventDefault();
+    }
     render(){
+        const {username,password} =this.state;
         return(
-        <div>Login Page</div>
+            <form onSubmit={this.handleSubmit}>           
+            <h2>Login Page</h2>
+            <h3>User Name</h3>
+            <input type="text" value={username} name="username" onChange={this.handleClick}/>
+            <h3>password</h3>
+            <input type="password" value={password} name="password" onChange={this.handleClick}/>
+            <button>Submit</button>
+        </form>
         )
     }
 }

514078d494d26530cd38c5cd62b50feadb371725
IshantL <ishantlambhate@gmail.com>
2019-04-10 12:49:28 +0000

    adding pages and private route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Login from './LoginPage';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import PrivateRoute from './PrivateRoute';
+import HomePage from './Homepage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Login />
+      <Router>
+        <PrivateRoute exact path="/" component={HomePage} />
+       <Route path="/login" component={Login}/>
+      </Router>
       </div>
     );
   }

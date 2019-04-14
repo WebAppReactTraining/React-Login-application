f9f4ed09057e2ce89a9f4074e3572d374a80bb83
ishantl <ishantlambhate@gmail.com>
2019-04-14 22:43:43 +0530

    login page modification

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,29 +5,49 @@ class Login extends Component{
         super();
         this.state={
             username:'',
-            password:''
+            password:'',
+            submitted: false,
+            loading: false,
+            error: ''
         }
+
         this.handleClick = this.handleClick.bind(this);
         this.handleSubmit= this.handleSubmit.bind(this);
     }
       handleClick(event){
         console.log("in handleClick");
-        this.setState({username:event.target.value})
+        debugger;
+        const { name, value} = event.target;
+        this.setState({[name]:value})
     }
 
     handleSubmit =e =>{
+        debugger;
         console.log(e.target.value);
         e.preventDefault();
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+
+        // stop here if form is invalid
+        if (!(username && password)) {
+            return;
+        }
     }
     render(){
-        const {username,password} =this.state;
+        const {username, password, submitted} =this.state;
         return(
             <form onSubmit={this.handleSubmit}>           
             <h2>Login Page</h2>
             <h3>User Name</h3>
             <input type="text" value={username} name="username" onChange={this.handleClick}/>
+            {submitted && !username &&
+                            <div>Username is required</div>
+                        }
             <h3>password</h3>
             <input type="password" value={password} name="password" onChange={this.handleClick}/>
+            {submitted && !password &&
+                            <div>password is required</div>
+                        }
             <button>Submit</button>
         </form>
         )
